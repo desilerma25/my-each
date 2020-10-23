0fda885ad6087576b22149ae99a26d9083623715
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-23 16:55:30 -0500

    commit tree c40259e8be141fd2c238337171c89737cd4ba4c2

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+# calls on while 
+
+def my_each(array) # put argument(s) here, accept array as arg.
+   i = 0
+   while i < array.length # calls on while 
+     yield(array[i]) # takes value of each index/element in array
+    i = i + 1
+  end
+   array
+ end
+
+
+# iterates over each element
+# yields the correct element
+# can handle an empty collection
+# returned array is original
+# does not modify original
+# block is run n times
+# only passes a single element into the block at a time
